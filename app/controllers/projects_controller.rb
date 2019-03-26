012177ca9e2bbd0fc45ae9199df059072b333417
oneofthemoons <ootmoons@gmail.com>
2019-03-27 00:06:28 +0300

    trash clearing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -40,31 +40,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
-  def update
-	puts "UPDATE PROJECT ------------------------------->>>>>"
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /projects/1
-  # DELETE /projects/1.json
-  def destroy
-    @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,18 +1,9 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
-  # GET /todos
-  # GET /todos.json
-  def index
-    @todos = Todo.all
-  end
-
-  # GET /todos/1
-  # GET /todos/1.json
   def show
   end
 
-  # GET /todos/new
   def new
 	@project = Project.find(params[:project_id])
     @todo = Todo.new
@@ -27,46 +18,8 @@ class TodosController < ApplicationController
   def create
 	@project = Project.find(params[:project_id])
 	@todo = @project.todos.create(todo_params)
-	puts "URAAAAA"
-    # respond_to do |format|
-	# 	if @todo.save
-	# 		format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
-	# 		format.json { render :show, status: :created, location: @todo }
-	# 	else
-	# 		format.html { render :new }
-	# 		format.json { render json: @todo.errors, status: :unprocessable_entity }
-	# 	end
-	# end
-	
-	# @project.save
 
 	redirect_to root_path
-  end
-
-  # PATCH/PUT /todos/1
-  # PATCH/PUT /todos/1.json
-  def update
-	@project = Project.find(params[:project_id])
-    respond_to do |format|
-      if @todo.update(todo_params)
-        format.html { redirect_to root_path, notice: 'Todo was successfully updated.' }
-        format.json { render :show, status: :ok, location: @todo }
-      else
-        format.html { render :edit }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /todos/1
-  # DELETE /todos/1.json
-  def destroy
-	@project = Project.find(params[:project_id])
-    @todo.destroy
-    respond_to do |format|
-      format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
