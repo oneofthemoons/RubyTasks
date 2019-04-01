55ace6b0676374bb3ac4681803c27fb74d7a6469
oneofthemoons <ootmoons@gmail.com>
2019-04-01 20:55:52 +0300

    hrk fix3

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,30 +1,20 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :edit, :update, :destroy]
-
+	before_action :set_todo, only: [:show, :edit, :update, :destroy]
+  
 	def create
-		@project = Project.find(params[:project_id])
-		@todo = @project.todos.create(todo_params)
-
-		redirect_to root_path
+	  @project = Project.find(params[:project_id])
+	  @todo = @project.todos.create(todo_params)
+  
+	  redirect_to root_path
 	end
-
-	def update
-		@project = Project.find(params[:project_id])
-		@todo = @project.todos.find(params[:id])
-
-		if @todo.isCompleted == false do
-			@todo.isCompleted = true
-		else
-			@todo.isCompleted = false
-		end
-	end
-
+  
 	private
-		def set_todo
+	  def set_todo
 		@todo = Todo.find(params[:id])
-		end
-
-		def todo_params
+	  end
+  
+	  def todo_params
 		params.require(:todo).permit(:text, :isCompleted, :project_id)
-		end
-end
+	  end
+  end
+  
