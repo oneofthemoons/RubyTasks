38948121adfd362abb0c1bfdbdff53ffd4c2bf76
oneofthemoons <ootmoons@gmail.com>
2019-04-01 19:51:57 +0300

    strike isCompleted

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,19 +1,30 @@
 class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :edit, :update, :destroy]
 
-  def create
-	@project = Project.find(params[:project_id])
-	@todo = @project.todos.create(todo_params)
+	def create
+		@project = Project.find(params[:project_id])
+		@todo = @project.todos.create(todo_params)
 
-	redirect_to root_path
-  end
+		redirect_to root_path
+	end
 
-  private
-    def set_todo
-      @todo = Todo.find(params[:id])
-    end
+	def update
+		@project = Project.find(params[:project_id])
+		@todo = @project.todos.find(params[:id])
 
-    def todo_params
-      params.require(:todo).permit(:text, :isCompleted, :project_id)
-    end
+		if @todo.isCompleted == false do
+			@todo.isCompleted = true
+		else
+			@todo.isCompleted = false
+		end
+	end
+
+	private
+		def set_todo
+		@todo = Todo.find(params[:id])
+		end
+
+		def todo_params
+		params.require(:todo).permit(:text, :isCompleted, :project_id)
+		end
 end
